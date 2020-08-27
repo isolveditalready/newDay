3ed17e48a101fc3140fc139ba3f97f96516395d7
rlee.jw <rlee.jw@gmail.com>
2020-08-27 07:53:26 -0400

    sdf

--- PLAYGROUND/GO/playground/helloWorld/main2.go
+++ PLAYGROUND/GO/playground/helloWorld/main2.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
 )
@@ -8,8 +10,17 @@ import (
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, req *http.Request) {
 		w.Header().Set("Content-Type", "text/plain; charset=utf-8") // normal header
-		w.WriteHeader(http.StatusOK)
-		log.Println("Hello World")
+		d, err := ioutil.ReadAll(req.Body)
+		if err != nil {
+			//w.WriteHeader(http.StatusBadRequest)
+			//w.Write([]byte("Ooops"))
+			// instead of above 2
+			http.Error(w, "Oops", http.StatusBadRequest)
+			return
+		}
+		log.Printf("Data %s\n", d)
+		fmt.Fprintf(w, "Hello %s", d)
+
 	})
 	http.HandleFunc("/goodbye", func(http.ResponseWriter, *http.Request) {
 		log.Println("later World")
